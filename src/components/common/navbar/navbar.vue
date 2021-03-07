f4af6495bc035c809dfb5b9e9f3d7d7b27436740
jiezai-888 <github123456>
2021-03-07 11:48:07 +0800

    navbar

---
+++ src/components/common/navbar/navbar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="nav-bar">
+		<div class="left">
+			<slot name="left"></slot>
+		</div>
+		<div class="center">
+			<slot name="center"></slot>
+		</div>
+		<div class="right">
+			<slot name="right"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'navbar'
+	}
+</script>
+
+<style>
+	.nav-bar {
+		display: flex;
+		height: 2.75rem;
+		text-align: center;
+		line-height: 0.30rem;
+
+	}
+
+	.left,
+	.right {
+		width: 3.75rem;
+		/* border: 2px solid greenyellow; */
+	}
+
+	.center {
+		flex: 1;
+		/* border: 2px solid blue; */
+	}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-  	<h1>首页</h1>
-  </div>
+	<div id="home">
+		<!-- <h1>首页</h1> -->
+		<navbar class="homenav">
+			<div slot="center">
+				<h3>购物街</h3>
+			</div>
+		</navbar>
+	</div>
 </template>
 
 <script>
-  export default{
-    name:'home'
-  }
+	import navbar from '../../components/common/navbar/navbar.vue'
+	export default {
+		name: 'home',
+		components:{
+			navbar
+		}
+	}
 </script>
 
 <style>
+	.homenav{
+		background-color: orangered;
+		color: whitesmoke;
+	}
 </style>
