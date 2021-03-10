6b672b0e4d214864d74a709a0aaedeb385764756
jiezai-888 <github123456>
2021-03-10 21:56:38 +0800

    banner

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,7 @@
+import swiper from "./swiper.vue"
+import swiperItem from "./swiperItem.vue"
+
+export{
+	swiper,
+	swiperItem
+}
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,26 +2,37 @@
 	<div id="home">
 		<!-- <h1>首页</h1> -->
 		<navbar class="homenav">
-			<div slot="center">
+			<div slot="center"  v-for="">
 				<h3>购物街</h3>
 			</div>
 		</navbar>
+		<swiper>
+			<swiper-item v-for="item in banner">
+				<a href="#">
+					<img :src="item" alt="none">
+				</a>
+			</swiper-item>
+		</swiper>
 	</div>
 </template>
 
 <script>
 	import navbar from '../../components/common/navbar/navbar.vue'
 	import {getHomeMultidata} from '../../network/home.js' 
+	import{swiper,swiperItem} from '../../components/common/swiper/index.js'
+	
 	export default {
 		name: 'home',
 		components: {
-			navbar
+			navbar,
+			swiper,
+			swiperItem
 		},
-		// data() {
-		// 	return {
-		// 		result: null
-		// 	}
-		// },
+		data() {
+			return {
+				banner: []
+			}
+		},
 		//当home实例被创建时，就进行异步请求
 		created() {
 			//返回的是一个promise函数
@@ -29,7 +40,7 @@
 			getHomeMultidata().then(resdata => {
 				//在箭头函数中this指向其父,而不是它本身
 				console.log(resdata);
-				// this.result=resdata;
+				this.banner=resdata;
 			})
 		}
 	}
